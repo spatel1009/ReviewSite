e4400b9f4222c7f45527e15d9b68f0d0f273bc25
spatel1009 <spatel1009@gmail.com>
2018-11-30 18:07:06 -0500

    add data validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :reviews, dependent: :destroy
+  validates :first_name, :last_name, presence: true
 end
